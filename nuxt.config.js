6d8de44343a9b7786cef90ce42b080bcaa9234d6
kammy1996 <kamranmemon25@gmail.com>
2022-06-21 18:23:59 +0530

    added GA config in app.html remove all old

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,14 +37,10 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  googleAnalytics: {
-    id: "UA-228420175-1",
-  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     "@nuxtjs/fontawesome",
-    "@nuxtjs/google-analytics",
     [
       // Simple usage
       "@nuxtjs/vuetify",
